99bdab6a8693fffcd92fabf7e1fc0c5a959e0641
ProdamGaraj <prodgaraj@yandex.ru>
2023-05-24 17:46:13 +0300

    Rename done

--- backend/src/main/java/com/example/Caramelca/controllers/ProfessionController.java
+++ backend/src/main/java/com/example/Caramelca/controllers/ProfessionController.java
@@ -16,25 +16,25 @@ import org.springframework.web.bind.annotation.*;
 @PreAuthorize("hasAuthority('ADMIN')")
 public class ProfessionController {
 
-    private final ProfessionRepository employeeServiceRepository;
+    private final ProfessionRepository professionRepository;
 
     private final EmployeeRepository employeeRepository;
 
     private final ServiceRepository serviceRepository;
 
-    public ProfessionController(ProfessionRepository employeeServiceRepository, EmployeeRepository employeeRepository, ServiceRepository serviceRepository) {
-        this.employeeServiceRepository = employeeServiceRepository;
+    public ProfessionController(ProfessionRepository professionRepository, EmployeeRepository employeeRepository, ServiceRepository serviceRepository) {
+        this.professionRepository = professionRepository;
         this.employeeRepository = employeeRepository;
         this.serviceRepository = serviceRepository;
     }
 
     @GetMapping("/")
     public String profession(Model model) {
-        Iterable<Profession> employeeServices = employeeServiceRepository.findAll();
+        Iterable<Profession> professions = professionRepository.findAll();
         Iterable<Employee> employees = employeeRepository.findAll();
         Iterable<Procedure> services = serviceRepository.findAll();
 
-        model.addAttribute("employeeServices", employeeServices);
+        model.addAttribute("employeeServices", professions);
         model.addAttribute("employees", employees);
         model.addAttribute("services", services);
 
@@ -44,8 +44,8 @@ public class ProfessionController {
     @PostMapping("/add")
     public String professionAdd(@RequestParam Employee employee,
                                 @RequestParam Procedure service) {
-        Profession employeeService = new Profession(employee, service);
-        employeeServiceRepository.save(employeeService);
+        Profession profession = new Profession(employee, service);
+        professionRepository.save(profession);
         return "redirect:/profession";
     }
 
@@ -55,14 +55,14 @@ public class ProfessionController {
                                    Model model) {
         Iterable<Employee> employees = employeeRepository.findAll();
         Iterable<Procedure> services = serviceRepository.findAll();
-        Iterable<Profession> employeeServices = employeeServiceRepository.findAll();
+        Iterable<Profession> professions = professionRepository.findAll();
         if(employee != null) {
-            employeeServices = employeeServiceRepository.findByEmployee(employee);
+            professions = professionRepository.findByEmployee(employee);
         }
         if(service != null) {
-            employeeServices = employeeServiceRepository.findByService(service);
+            professions = professionRepository.findByService(service);
         }
-        model.addAttribute("employeeServices", employeeServices);
+        model.addAttribute("employeeServices", professions);//TODO: ебануть то же на фронте но т.к. реакт похуй
         model.addAttribute("employees", employees);
         model.addAttribute("services", services);
         return "profession";
@@ -70,7 +70,7 @@ public class ProfessionController {
 
     @PostMapping("/delete/{id}")
     public String professionDelete(@PathVariable(value = "id") Long id) {
-        employeeServiceRepository.deleteById(id);
+        professionRepository.deleteById(id);
         return "redirect:/profession";
     }
 
