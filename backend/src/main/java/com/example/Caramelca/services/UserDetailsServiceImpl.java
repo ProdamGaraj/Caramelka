2a401410c90b9fbf443119a72436aa55d194f07d
SAPIPA <lbvf20035@gmail.com>
2023-05-27 03:32:12 +0300

    Add anotation to UserDetailsServiceImpl and AuthEntryPointJwt

--- backend/src/main/java/com/example/Caramelca/services/UserDetailsServiceImpl.java
+++ backend/src/main/java/com/example/Caramelca/services/UserDetailsServiceImpl.java
@@ -6,7 +6,9 @@ import jakarta.transaction.Transactional;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     UserRepository userRepository;
--- backend/src/main/java/com/example/Caramelca/utils/AuthEntryPointJwt.java
+++ backend/src/main/java/com/example/Caramelca/utils/AuthEntryPointJwt.java
@@ -7,9 +7,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+@Component
 public class AuthEntryPointJwt implements AuthenticationEntryPoint {
     private static final Logger logger = LoggerFactory.getLogger(AuthEntryPointJwt.class);
 
